b86e410df3fb8655953399b0b7c04ec2c956277f
Ravinder Singh <rsingh@ravi-singh.com>
2021-12-22 13:11:55 +0000

    Multiple results function example

--- functions.go
+++ functions.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
   fmt.Println(getMessage("Foo"))
   fmt.Println(add(23, 32))
+  a, b := swap("hello", "world")
+  fmt.Println(a, b)
 }
 
 func getMessage(name string) string {
@@ -15,3 +17,7 @@ func add(x, y int) int {
   return x + y
 }
 
+// multiple results function
+func swap(a, b string) (string, string) {
+  return b, a
+}
