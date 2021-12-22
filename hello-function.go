dfc913140b59a041ac28cb775e4d4e5ef590c077
Ravinder Singh <rsingh@ravi-singh.com>
2021-12-22 02:39:35 +0000

    Functions'

--- hello-function.go
+++ hello-function.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(getMessage("Ravi"))
+  fmt.Println(getMessage("Foo"))
+  fmt.Println(add(23, 32))
 }
 
 func getMessage(name string) string {
   return "Hello " + name
 }
+
+func add(x, y int) int {
+  return x + y
+}
+
